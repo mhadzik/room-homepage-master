3f5d28d7e4bd6b83142479add85855b82125b1ad
Mikolaj Hadzik <mikolaj.hadzik@gmail.com>
2021-07-05 23:59:48 +0200

    Setup dev/prod environment

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "dist"),
+    publicPath: "/", //should provide the path of the served js , img , etc...
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.html$/i,
+        use: [{ loader: "html-loader", options: { minimize: true } }],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-sprite-loader",
+        options: {
+          extract: true,
+          spriteFilename: "svg-defs.svg",
+        },
+      },
+      {
+        test: /\.(jpe?g|png|gif)\$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images/",
+              name: "[name].[ext]",
+            },
+          },
+          "img-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};
